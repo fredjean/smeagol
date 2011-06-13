56b77bd62fb9b1fb4901ec4dded4aced6cc29596
Frederic Jean <fred@fredjean.net>
2011-06-12 21:47:19 -0600

    Adapting recipes.

--- cookbooks/homebrew/recipes/dbs.rb
+++ cookbooks/homebrew/recipes/dbs.rb
@@ -8,8 +8,8 @@ root = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 require root + '/resources/homebrew'
 require root + '/providers/homebrew'
 
-homebrew_db "redis"
-homebrew_db "mongodb"
+# homebrew_db "redis"
+# homebrew_db "mongodb"
 homebrew_db "memcached"
 homebrew_db "postgresql"
 homebrew_db "mysql"
